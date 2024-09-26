4f4a747c8c81807853fd826052bfc08c108eb093
Daniel-Silva-Berbert <magnoxsbr@gmail.com>
2024-09-26 16:58:56 -0300

    feat #Daniel: Documentando as rotas do Cargo

--- crud/src/main/java/br/com/jupiter/crud/controller/CargoController.java
+++ crud/src/main/java/br/com/jupiter/crud/controller/CargoController.java
@@ -7,9 +7,14 @@ import br.com.jupiter.crud.entity.Cargo;
 import br.com.jupiter.crud.service.CargoService;
 import br.com.jupiter.crud.service.exception.EntityNotFoundException;
 import br.com.jupiter.crud.service.exception.NameNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
@@ -24,12 +29,18 @@ public class CargoController {
     this.cargoService = cargoService;
   }
 
+  @Operation(summary = "Retorna todos os cargos.")
   @GetMapping
   public List<CargoDto> getAll() {
     List<Cargo> allCargos = cargoService.getAll();
     return allCargos.stream().map(CargoDto::fromEntity).toList();
   }
 
+  @Operation(summary = "Retorna um cargo especifico com base no ID.", description = "Retorna um cargo específico com base no ID fornecido como parâmetro.")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "ID do cargo encontrado"),
+    @ApiResponse(responseCode = "404", description = "ID do cargo não encontrado")
+  })
   @GetMapping("/{id}")
   public CargoDto getById(@PathVariable Long id) throws EntityNotFoundException {
     return CargoDto.fromEntity(
@@ -37,11 +48,17 @@ public class CargoController {
     );
   }
 
+  @Operation(summary = "Retorna um cargo especifico com base na String.", description = "Retorna um cargo específico com base na String fornecido como parâmetro no search.")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Nome do cargo encontrado"),
+    @ApiResponse(responseCode = "404", description = "Nome do cargo não encontrado")
+  })
   @GetMapping("search")
   public List<Cargo> getbyName(@RequestParam String cargo) throws NameNotFoundException {
     return cargoService.getByName(cargo);
   }
 
+  @Operation(summary = "Criar um novo cargo.")
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public CargoDto salvar(@RequestBody CargoCreationDto cargoCreationDto) {
@@ -50,6 +67,7 @@ public class CargoController {
     );
   }
 
+  @Operation(summary = "Atualizar um cargo.", description = "Atualizar um cargo com base no ID fornecido como parâmetro.")
   @PutMapping("/{id}")
   public CargoDto editar(@PathVariable Long id, @RequestBody CargoCreationDto cargoCreationDto) throws EntityNotFoundException {
     return CargoDto.fromEntity(
@@ -57,6 +75,7 @@ public class CargoController {
     );
   }
 
+  @Operation(summary = "Deleta um cargo.", description = "Deleta um cargo com base no ID fornecido como parâmetro.")
   @DeleteMapping("/{id}")
   public CargoDto delete(@PathVariable Long id) throws EntityNotFoundException {
     return CargoDto.fromEntity(
